df69cc33d8e0d58274fd36cb295e6f0a919678c3
Aryan Bhati <aryanbhati9638@gmail.com>
2024-06-30 11:13:21 +0530

    home section added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
 
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return <p>{title}</p>;
+};
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] "
+      >
+        I'm a skilled software developer with experience in React, Node.js, and
+        MongoDB. I have a strong foundation in computer science and programming
+        concepts across multiple platforms. I'm passionate about creating
+        software that is scalable, maintainable, and efficient. I'm always
+        looking to learn new technologies and improve my skills. I'm a team
+        player who enjoys collaborating with others to solve complex problems
+        and deliver high-quality software solutions.
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10 ">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
