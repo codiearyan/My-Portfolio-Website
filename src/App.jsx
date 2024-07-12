6be73f01e674b44904cbf9452eda8ea10bcb961a
Aryan Bhati <aryanbhati9638@gmail.com>
2024-07-12 15:22:52 +0530

    error debug

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         </div>
         <About />
         {/* <Experience /> */}
-        <Tech />
+        {/* <Tech /> */}
         <Works />
         {/* <Feedbacks /> */}
         <div className="relative z-0">
